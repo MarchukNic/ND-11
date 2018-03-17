d36dc9af82f8d40d2498e4da2ef4be02d76d663f
MarchukNic <killer-nic@yandex.ru>
2018-03-17 23:57:41 +0300

    1

--- read-all.js
+++ read-all.js
@@ -1,16 +1,25 @@
 const fs = require('fs');
 
 function readAll(path){
-    return new Promise((done, fail, name) => {
-        fs.readdir(path, (err, files, name) => {
+    
+    return new Promise((done, fail) => {
+        fs.readdir(path, (err, files) => {
             if (err) {
                 fail(err);
             } else {
-                done(files);
-                name('files.name');
+                //file = {name: files}
+                let file = [{ name: 'Петя', content: 'dsfssfsdf' },{ name: 'Петя', content: 'dsfssfsdf' },{ name: 'Петя', content: 'dsfssfsdf' }];
+                console.log(file);
+                console.log(files);
+                //done(files);
+                done(file);
+                //done([{ name: 'Петя', content: 'dsfssfsdf' },{ name: 'Петя', content: 'dsfssfsdf' },{ name: 'Петя', content: 'dsfssfsdf' }]);
             };
+            //console.log(files);
         });
+        //console.log(file.name);
     });
+    //console.log(file.name);
 };
 
 module.exports = readAll;
