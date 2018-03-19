b0baadf565a1f8cb935effd2fb8e521be0df1d13
MarchukNic <killer-nic@yandex.ru>
2018-03-19 10:26:08 +0300

    закончено

--- Lesson01/read-all.js
+++ Lesson01/read-all.js
@@ -1,3 +1,4 @@
+const file = require('./file-promise');
 const fs = require('fs');
 
 function readAll(path){
@@ -6,28 +7,36 @@ function readAll(path){
             if (err) {
                 fail(err);
             } else {
-                console.log(files[0]);
-                let file2 = {};
-                for (let i = 0; files.length - 1; ++i){
-                    file2.name = files[i];
-                    file2.content = files[i];
-                    files[i] = file2;
-                }
-                console.log(file2);
-                //done(filesContent(files));
-                done(files);
+                // console.log(files[0]);
+                // let file2 = {};
+                // for (let i = 0; files.length - 1; ++i){
+                //     file2.name = files[i];
+                //     file2.content = files[i];
+                //     files[i] = file2;
+                // }
+                // console.log(file2);
+                done(filesContent(files,path));
+                // done(files);
             };
         });
     });
 };
 
-function filesContent(files){
-    let file = [{ name: 'Петя', content: 'dsfssfsdf' },{ name: '2Петя', content: '2dsfssfsdf' },{ name: '3Петя', content: '3dsfssfsdf' }];
-    file[0].name = 'bujkm';
-    console.log(file);
-    console.log(file[0].name)
-    console.log(files);
-    return file;
-}
+
+const filesContent = (itemIds, path) => Promise.all(
+    itemIds.map(
+        name => {
+            console.log(path + name);
+            return file.read(path + name)
+                .then(content => {
+                    console.log({ content, name });
+                    return { content, name }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    )
+)
 
 module.exports = readAll;
