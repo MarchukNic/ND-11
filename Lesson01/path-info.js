5c78231417a9ac9d50c6e19973679a3922923cf9
MarchukNic <killer-nic@yandex.ru>
2018-03-22 11:54:34 +0300

    исправление на callback(err)

--- Lesson01/path-info.js
+++ Lesson01/path-info.js
@@ -6,12 +6,12 @@ function pathInfo(path, callback) {
     info.path = path;
     fs.stat(path, getType);
     function getType(err, stats) {
-        if (err) return err;
+        if (err) return callback(err);
         if (stats.isFile()) {
             info.type = 'file';
             fs.readFile(path, conf, fileRead);
             function fileRead(err, content) {
-                if (err) return (err);
+                if (err) return callback(err);
                 info.content = content;
                 return callback(null, info);
             };
@@ -19,7 +19,7 @@ function pathInfo(path, callback) {
             info.type = 'directory';
             fs.readdir(path, readAll);
             function readAll(err, files) {
-                if (err) return (err);
+                if (err) return callback(err);
                 info.childs = files;
                 return callback(null, info);
             };
