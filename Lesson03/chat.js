e1fa91ed2a5361211578f81a7c99fc0bd166f984
MarchukNic <killer-nic@yandex.ru>
2018-03-24 18:31:42 +0300

    драфт 33го задания

--- Lesson03/chat.js
+++ Lesson03/chat.js
@@ -12,32 +12,56 @@ class ChatApp extends EventEmitter {
     // Посылать каждую секунду сообщение
     setInterval(() => {
       this.emit('message', `${this.title}: ping-pong`);
-  }, 1000);
+    }, 1000);
+  
+    // Часть 2.1 Добавляем метод close
+    let close = this.close;
   }
 }
 
-let webinarChat =  new ChatApp('webinar');
+let webinarChat = new ChatApp('webinar');
 let facebookChat = new ChatApp('=========facebook');
-let vkChat =       new ChatApp('---------vk');
+let vkChat = new ChatApp('---------vk');
 
 let chatOnMessage = (message) => {
   console.log(message);
 };
 
+// Часть 1.1 Начало. Добавляем обработчик события
+let chatOnMessageReady = () => {
+  console.log('Готовлюсь к ответу');
+};
+
+// Добавляем обработчик для webinarChat
+webinarChat.on('message', chatOnMessageReady);
+// Часть 1.1 Конец
+
 webinarChat.on('message', chatOnMessage);
 facebookChat.on('message', chatOnMessage);
 vkChat.on('message', chatOnMessage);
 
+// Часть 1.2 Устанавливаем максимальное количество обработчиков 2
+vkChat.setMaxListeners(2);
+
+// Часть 1.3 Добавляем обработчик vkChat
+vkChat.on('message', chatOnMessageReady);
+
 
 // Закрыть вконтакте
-setTimeout( ()=> {
+setTimeout(() => {
   console.log('Закрываю вконтакте...');
-vkChat.removeListener('message', chatOnMessage);
-}, 10000 );
+  vkChat.removeListener('message', chatOnMessage);
+  
+  // Часть 2.2 
+  vkChat.on('close',chatOnMessage('Чат вконтакте закрылся :('));
+
+  // Часть 2.3
+  vkChat.close();
+}, 10000);
 
 
 // Закрыть фейсбук
-setTimeout( ()=> {
+setTimeout(() => {
   console.log('Закрываю фейсбук, все внимание — вебинару!');
-facebookChat.removeListener('message', chatOnMessage);
-}, 15000 );
+  facebookChat.removeListener('message', chatOnMessage);
+}, 15000);
