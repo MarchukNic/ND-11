f2cb125a45a23c2bcab3d93f901c6957d0638c8d
MarchukNic <killer-nic@yandex.ru>
2018-04-05 09:38:06 +0300

    готово

---
+++ Lesson2_2/homeWork.js
@@ -0,0 +1,118 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+let users = [
+    {
+        // "id": 1,
+        "name": "John",
+        "score": 4
+    },
+    {
+        // "id": 2,
+        "name": "Tom",
+        "score": 8
+    },
+    {
+        // "id": 3,
+        "name": "Tom",
+        "score": 5
+    }
+];
+
+app.get('/users/', (req, res) => {
+    res.status(200);
+    res.json(users.filter(u => u))
+});
+
+app.post('/users/', (req, res) => {
+    users.push(req.body)
+    res.status(200);
+    res.send()
+});
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+    if (users[id]) {
+        users[id] = null;
+        res.status(200);
+        res.send();
+    } else {
+        res.status(400);
+        res.write('Данного пользователя нет!');
+        res.send();
+    }
+});
+
+// app.get('/users/:id', (req, res) => {
+//     const id = req.params.id;
+//     let item = users.find(item => item.id == id);
+//     if (item !== undefined) {
+//         res.json(item);
+//     } else {
+//         res.status(404);
+//         res.send();
+//     }
+// });
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    if (users[id]) {
+        res.json(users[id]);
+    } else {
+        res.status(400);
+        res.write('Данного пользователя нет!');
+        res.send();
+    }
+});
+
+app.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    if (users[id]) {
+        users[id] = Object.assign(users[id], req.body);
+        res.status(200);
+        res.json(users[id]);
+    } else {
+        res.status(400);
+        res.write('Данного пользователя нет!');
+        res.send();
+    }
+});
+
+var RPC = {
+    speak: function (callback) {
+        callback(null, 'meow');
+    }
+};
+
+//http://www.jsonrpc.org/specification#examples
+app.post("/rpc", function (req, res) {
+    console.log(req.body.method);
+    console.log(req.body.params);
+    const method = RPC[req.body.method];
+    // method(req.body.params, function (error, result) {
+    if (method !== undefined) {
+    method(function (error, result) {
+        if (error) {
+            res.status(400);
+            res.write('Ошибка запроса!');
+            res.send();
+        }
+        else {
+            res.status(200);
+            res.json(result);
+        }
+    })}
+    else {
+        res.status(400);
+            res.write('Такого метода не существует!');
+            res.send();
+    }
+});
+//https://bcb.github.io/jsonrpc/node
+//https://www.npmjs.com/package/node-json-rpc
+
+app.listen(3000);
