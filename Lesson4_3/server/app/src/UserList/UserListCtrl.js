d0c5a0d6766082254d0764654567bb9a770f609c
MarchukNic <killer-nic@yandex.ru>
2018-07-05 19:52:53 +0300

    upd comment

--- Lesson4_3/server/app/src/UserList/UserListCtrl.js
+++ Lesson4_3/server/app/src/UserList/UserListCtrl.js
@@ -1,16 +1,19 @@
 'use strict'
 
 userApp.controller('UserListCtrl', function ($scope, UsersService, PostsService) {
+  // Добавить лоадеры в список пользователей и постов.
   $scope.userLoadedU = false
   $scope.userLoadedP = false
 
   UsersService.getUsers().then(function (response) {
     $scope.users = response.data
+    // Добавить лоадеры в список пользователей и постов.
     $scope.userLoadedU = true
   })
 
   PostsService.getPosts().then(function (response) {
     $scope.posts = response.data
+    // Добавить лоадеры в список пользователей и постов.
     $scope.userLoadedP = true
   })
 
