e4c7e0e201a285acbeb6b08a5a8b99279246bf7b
MarchukNic <killer-nic@yandex.ru>
2018-07-06 08:08:00 +0300

    4.3 доп задание $q.all(...).

--- Lesson4_3/server/app/src/UserList/UserListCtrl.js
+++ Lesson4_3/server/app/src/UserList/UserListCtrl.js
@@ -1,28 +1,38 @@
 'use strict'
 
-userApp.controller('UserListCtrl', function ($scope, UsersService, PostsService) {
+userApp.controller('UserListCtrl', function ($scope, UsersService, PostsService, $q) {
   // Добавить лоадеры в список пользователей и постов.
   $scope.userLoadedU = false
   $scope.userLoadedP = false
-
-  UsersService.getUsers().then(function (response) {
-    $scope.users = response.data
-    // Добавить лоадеры в список пользователей и постов.
-    $scope.userLoadedU = true
-  })
-
-  PostsService.getPosts().then(function (response) {
-    $scope.posts = response.data
-    // Добавить лоадеры в список пользователей и постов.
-    $scope.userLoadedP = true
-  })
-
-
-/*   UsersService.getUsers().then(function (response) {
-    $scope.users = response.data
-    return PostsService.getPosts()
+  var obj1, obj2
+  $q.all({
+    obj1: UsersService.getUsers(),
+    obj2: PostsService.getPosts()
   }).then(function (response) {
-    $scope.posts = response.data
-  }) */
+    $scope.users = response.obj1.data;
+    $scope.posts = response.obj2.data;
+    $scope.userLoadedU = true;
+    $scope.userLoadedP = true;
+  });
+
+  // UsersService.getUsers().then(function (response) {
+  //   $scope.users = response.data
+  //   // Добавить лоадеры в список пользователей и постов.
+  //   $scope.userLoadedU = true
+  // })
+
+  // PostsService.getPosts().then(function (response) {
+  //   $scope.posts = response.data
+  //   // Добавить лоадеры в список пользователей и постов.
+  //   $scope.userLoadedP = true
+  // })
+
+
+  /*   UsersService.getUsers().then(function (response) {
+      $scope.users = response.data
+      return PostsService.getPosts()
+    }).then(function (response) {
+      $scope.posts = response.data
+    }) */
 
 })
